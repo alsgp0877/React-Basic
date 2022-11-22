d51f569b42cb6e4b2ef5166f9249f1d80ca21bec
alsgp0877 <alsgp0877@naver.com>
2022-11-22 23:40:51 +0900

    이벤트 onClick 사용

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state={
+      mode:'read',
       subject:{title:'Xeb',sub:'Xorld Xide Xeb!'},
+      welcome:{title:'Welcome',desc:'Hello, React!!'},
       contents:[
         {id:1,title:'HTML', desc:'HTML is HyperText..'},
         {id:2,title:'CSS', desc:'CSS is HyperText..'},
@@ -18,12 +20,24 @@ class App extends Component{
   }
 
   render(){
+    var _title,_desc = null;
+    if(this.state.mode == 'welcome'){
+      _title=this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else if(this.state.mode==='read'){
+      _title=this.state.contents[0].title;
+      _desc=this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> */}
+        <header>
+        <h1><a href="/" onClick={function(e){alert('HI'); e.preventDefault();}}>{this.state.subject.title}</a></h1>
+        {this.state.subject.sub}
+        </header>
         <Subject title="REACT" sub="world wide web!"></Subject>
         <TOC data={this.state.contents}></TOC>
-        <CONT title="a;dfjk;adjkf" desc="adsfad"></CONT>
+        <CONT title={_title} desc={_desc}></CONT>
       </div>
     );
   }
