aeca67af4e07f66468493e389d678a9c16c5b54f
mhchoi <alsgp0877@naver.com>
2022-12-12 23:29:57 +0900

    update 1

---
+++ src/components/UpdateContent.js
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+
+
+class UpdateContent extends Component {
+  render(){
+    console.log(this.props.data);
+    console.log('UpdateContetn render');
+    return (
+      <article>
+        <h2>Update</h2>
+        <form action="/create_process" method="post" 
+          onSubmit={function(e){
+            e.preventDefault();
+            debugger;
+            this.props.onSubmit(e.target.title.value,e.target.desc.value);
+            alert('submit!!!');
+          }.bind(this)}>
+          <p><input type="text" name="title" placeholder="title"></input></p>
+          <p><textarea name="desc" placeholder="description"></textarea></p>
+          <p><input type="submit"></input></p>
+        </form>
+      </article>
+    );
+  }
+}
+
+
+export default UpdateContent;
