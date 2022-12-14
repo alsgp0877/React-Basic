76750d166b970090155049000e3bf5f799264851
mhchoi <alsgp0877@naver.com>
2022-12-14 21:54:54 +0900

    update시 props 말고 state 사용하기

--- src/components/UpdateContent.js
+++ src/components/UpdateContent.js
@@ -2,9 +2,24 @@ import { Component } from 'react';
 
 
 class UpdateContent extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      title:this.props.data.title,
+      desc:this.props.data.desc
+    }
+    this.inputFormHanlder = this.inputFormHanlder.bind(this);
+
+  }
+
+  inputFormHanlder(e){
+    this.setState({[e.target.name]:e.target.value});
+  }
+
+
   render(){
     console.log(this.props.data);
-    console.log('UpdateContetn render');
+    console.log('UpdateContent render');
     return (
       <article>
         <h2>Update</h2>
@@ -15,9 +30,26 @@ class UpdateContent extends Component {
             this.props.onSubmit(e.target.title.value,e.target.desc.value);
             alert('submit!!!');
           }.bind(this)}>
-          <p><input type="text" name="title" placeholder="title"></input></p>
-          <p><textarea name="desc" placeholder="description"></textarea></p>
-          <p><input type="submit"></input></p>
+          <p>
+            <input 
+            type="text" 
+            name="title" 
+            placeholder="title" 
+            value={this.state.title}
+            onChange = {this.inputFormHanlder} 
+            ></input>
+            {/* props는 readonly인데 수정을 하려고 하면 오류가 난다 그래서 state 형으로 변환? 해야한다 */}
+            </p>
+          <p>
+            <textarea 
+            onChange = {this.inputFormHanlder} 
+            name="desc" 
+            placeholder="description" 
+            value={this.state.desc}></textarea>
+            </p>
+          <p>
+            <input type="submit"></input>
+          </p>
         </form>
       </article>
     );
